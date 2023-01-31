fcce9e7483023c38a3cf32371ba25d7494f75203
NishuDissanayake <nishudissanayake5252@gmail.com>
2023-01-31 15:38:05 +0530

    Resources testing

--- test/resources.test.js
+++ test/resources.test.js
@@ -29,6 +29,12 @@ describe('Resources Test Collection!', () => {
     it('Test resources POST operation: ', (done) => {
 
         let resource= {
+            Email: 'test@testmail.com',
+            ResourcesType: 'Software',
+            ResourcesName: 'Redmine permission',
+            Description: 'Added to selected workspaces',
+            IssuedDate: '2022-02-12',
+            Status: 'Active',
         }
 
         chai.request(server)
@@ -57,6 +63,74 @@ describe('Resources Test Collection!', () => {
 
         chai.request(server)
             .get('/resources/all')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(1);
+                done();
+            });
+    });
+
+    it('Add 1 more record for filtering: ', (done) => {
+
+        let resource= {
+            Email: 'test2@testmail.com',
+            ResourcesType: 'Hardware',
+            ResourcesName: 'Mouse',
+            Description: 'Logitech wireless',
+            IssuedDate: '2022-02-14',
+            Status: 'Not returned',
+        }
+
+        chai.request(server)
+            .post('/resources/add')
+            .send(resource)
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
+
+    it('Test Software resources GET by email operation: ', (done) => {
+
+        chai.request(server)
+            .get('/resouces/software/by-email?email=test@testmail.com')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(1);
+                done();
+            });
+    });
+
+    it('Test Hardware resources GET by email operation: ', (done) => {
+
+        chai.request(server)
+            .get('/resouces/hardware/by-email?email=test2@testmail.com')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(1);
+                done();
+            });
+    });
+
+    it('Test Hardware resources GET all operation: ', (done) => {
+
+        chai.request(server)
+            .get('/resouces/software/all')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(1);
+                done();
+            });
+    });
+
+    it('Test Hardware resources GET all operation: ', (done) => {
+
+        chai.request(server)
+            .get('/resouces/hardware/all')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
